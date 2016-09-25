96d41c221dee18d1e4e2dd81b985fbbd0493c145
Makarov, Ilya <ilya.makarov@x5.ru>
2016-09-25 13:01:30 +0300

    rm fix2 because of bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 set -o pipefail -ue
-SHELLCHECK_EXCLUDE=${SHELLCHECK_EXCLUDE:=""}
+
+readonly SHELLCHECK_EXCLUDE=${SHELLCHECK_EXCLUDE:=""}
+readonly DEBUG=${DEBUG:="false"}
 
 if [ ! -f "/tmp/FileToBeChecked" ]; then
     echo "File not found /tmp/FileToBeChecked"
     exit 1
 fi
 
-tr -cd '\11\12\15\40-\176' < /tmp/FileToBeChecked > /tmp/fixfile2
-LC_CTYPE=C sed '1s/^...//' < /tmp/fixfile2 > /tmp/fixfile
+tr -cd '\11\12\15\40-\176' < /tmp/FileToBeChecked > /tmp/fixfile
+if [[ ${DEBUG} -eq 'true' ]]; then
+   cat /tmp/fixfile 
+fi
 
 shellcheck -e "${SHELLCHECK_EXCLUDE}" /tmp/fixfile
